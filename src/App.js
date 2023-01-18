435b9ccd5cca3c0d7bf8309f37129456102b14ba
edenbunes <115397221+edenbunes@users.noreply.github.com>
2023-01-18 12:46:43 +0200

    Added a link to GitHub

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Search />
+      <br />
+      <small>
+        <a
+          href="https://github.com/edenbunes/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Eden Bunes
+      </small>
     </div>
   );
 }
