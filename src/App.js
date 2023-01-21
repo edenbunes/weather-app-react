f6e4f26f583d98ebbd9306bf5c3370d84e7a4b37
edenbunes <115397221+edenbunes@users.noreply.github.com>
2023-01-21 10:31:52 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import Search from "./Search";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Search />
-      <br />
-      <small>
-        <a
-          href="https://github.com/edenbunes/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by Eden Bunes
-      </small>
+      <div cllasNane="container">
+        <h1>Weather App</h1>
+        <Search />
+        <br />
+        <small>
+          <a
+            href="https://github.com/edenbunes/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Eden Bunes
+        </small>
+      </div>
     </div>
   );
 }
